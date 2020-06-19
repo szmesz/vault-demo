52fdec3d8cab0e36f4c6d12e35db833efad1ccfe
Vajdat <32224054+szmesz@users.noreply.github.com>
2020-06-19 12:17:35 +0200

    clean up code

--- src/main/java/com/epam/vaultdemo/VaultDemoApplication.java
+++ src/main/java/com/epam/vaultdemo/VaultDemoApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VaultDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VaultDemoApplication.class, args);
     }
-
 }
